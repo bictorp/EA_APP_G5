e4ebb6743e1f26cde4a6f7d73b6124971f649f1c
0mar089 <omar.boury@estudiantat.upc.edu>
2026-05-12 00:33:09 +0200

    fixed: variable api url

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,22 +1,7 @@
-import 'dart:io' show Platform;
-import 'package:flutter/foundation.dart' show kIsWeb, kReleaseMode;
-
 class ApiConstants {
-  static String get baseUrl {
-    // Si la app está compilada para producción (Release Mode)
-    if (const bool.fromEnvironment('dart.vm.product') || kReleaseMode) {
-      return 'https://tu-servidor-produccion.com'; // CAMBIAR AQUÍ PARA PRODUCCIÓN
-    }
+  static const String _url = 'http://192.168.1.24:1337';
 
-    if (kIsWeb) {
-      return 'http://localhost:1337';
-    } else if (Platform.isAndroid) {
-      // Usamos tu IP local que sabemos que funciona en tu entorno
-      return 'http://192.168.1.24:1337';
-    } else {
-      return 'http://localhost:1337';
-    }
-  }
+  static String get baseUrl => _url;
 
   static String get loginEndpoint => '$baseUrl/auth/login';
   static String get registerEndpoint => '$baseUrl/auth/register';
