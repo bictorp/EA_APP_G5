c30d8c3112f00c6f23e4f861a4f66fae0b8a901f
jannogueira <jannogueiraacosta@gmail.com>
2026-06-21 21:31:57 +0200

    Production API URL

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,16 +1,5 @@
-import 'package:flutter/foundation.dart';
-
 class ApiConstants {
-  // static const String _url = 'http://ea5-api.upc.edu'; // Production URL
-  static String get _url {
-    if (kIsWeb) {
-      return 'http://localhost:1337';
-    }
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://10.0.2.2:1337';
-    }
-    return 'http://localhost:1337';
-  }
+  static const String _url = 'http://ea5-api.upc.edu';
 
   static String get baseUrl => _url;
 
